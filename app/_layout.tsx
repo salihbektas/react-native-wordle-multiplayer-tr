2bbe64f6d6068132e671fa968aa46061b9c656a1
salihbektas <sbektas@gtu.edu.tr>
2024-08-06 23:45:33 +0300

    safeAreaView added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,26 @@
+import { colors } from '@/constants/Colors';
 import { toastConfig } from '@/constants/toastConfig';
 import { store } from '@/store/store';
 import { Slot } from 'expo-router';
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
 import { Provider } from 'react-redux';
 
 export default function Layout() {
   return(
     <Provider store={store}>
-      <Slot />
-      <Toast config={toastConfig} />
+      <SafeAreaView style={styles.safeArea}>
+        <Slot />
+        <Toast config={toastConfig} />
+      </SafeAreaView>
     </Provider>
   )
 }
+
+const styles = StyleSheet.create({
+  safeArea : {
+    flex:1,
+    backgroundColor: colors.black
+  }
+})
