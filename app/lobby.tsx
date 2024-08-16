4afe5144fbf868c469d0fa0b413a1a4c4ccb0d50
salihbektas <sbektas@gtu.edu.tr>
2024-08-17 01:28:56 +0300

    added player kick feature

--- app/lobby.tsx
+++ app/lobby.tsx
@@ -4,7 +4,7 @@ import dbRootRef, { ServerType } from "@/utils/firebase"
 import { router } from "expo-router"
 import { child, off, onValue, remove, runTransaction, update } from "firebase/database"
 import { useEffect, useState } from "react"
-import { Pressable, StyleSheet, Text, View } from "react-native"
+import { Alert, Image, Platform, Pressable, StyleSheet, Text, View } from "react-native"
 import { useDispatch, useSelector } from "react-redux"
 import { WORDSLENGTH} from '../constants/constants'
 import { addWords } from "@/features/playerSlice/playerSlice"
@@ -29,6 +29,7 @@ export default function lobby() {
   }
 
   useEffect(() => {
+    const dbRef = child(dbRootRef, dbRefName)
 
     if(amIHost){
       let i = 0
@@ -42,12 +43,16 @@ export default function lobby() {
       }while(i < defaultConfigs.wordsCount);
       
       const updates = {answers: tempAnswerIndexes}
-      update(child(dbRootRef, dbRefName), updates)
+      update(dbRef, updates)
     }
 
-    const unsubscribe = onValue(child(dbRootRef, dbRefName), snapshot => {
+    const unsubscribe = onValue(dbRef, snapshot => {
       if(snapshot.exists()){
         const data = snapshot.val()
+        if(!(data.playerList as string[]).find(playerNameInList => playerName === playerNameInList)){
+          off(dbRef)
+          router.back()
+        }
         setPlayerList(data.playerList)
         dispatch(addWords(data.answers))
         
@@ -85,6 +90,32 @@ export default function lobby() {
     }
   }
 
+  function kickPlayer(playerName: string) {
+    function kick(params:string) {
+      runTransaction(child(dbRootRef, dbRefName), (serverState:ServerType) => {
+        if(serverState){
+          serverState.playerCount--
+          serverState.playerList = serverState.playerList.filter(playerNameOnList => params !== playerNameOnList)
+          delete serverState.results[params]
+          delete serverState.points[params]
+        }
+        return serverState;
+      })
+    }
+    
+    if(Platform.OS === 'web' && confirm(`${playerName} oyuncusunu atmak istiyormusunuz ?`)){
+      kick(playerName)
+    }
+    else{
+      Alert.alert(`${playerName} oyuncusunu atmak istiyormusunuz ?`,'',
+        [
+          {text: 'Hayır', style:'cancel'},
+          {text: 'Evet', onPress: () => { kick(playerName) }}
+        ]
+      )
+    }
+  }
+
   return(
     <View style={styles.main}>
       <Text style={styles.text}>Oyun Lobisi</Text>
@@ -94,9 +125,13 @@ export default function lobby() {
       <View style={styles.listContainer}>
       
       <Text style={styles.text} >Oyuncular...</Text>
-      {playerList.map((playerName, index) => 
+      {playerList.map((playerNameInList, index) => 
         <View key={index} style={styles.listRow} >
-          <Text style={styles.text} >{playerName}</Text>
+          <Text style={styles.text} >{playerNameInList}</Text>
+          {amIHost && playerName !== playerNameInList && <Pressable onPress={() => {kickPlayer(playerNameInList)}}>
+            <Image source={require('../assets/images/close.png')} style={styles.icon} />
+          </Pressable>
+          }
         </View>
       )}
 
@@ -145,6 +180,15 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: colors.lightGray,
     flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
+  icon: {
+    height: 36,
+    width: 36,
+    marginTop: 3,
+    resizeMode: 'cover',
+    tintColor: colors.white,
   },
 
   button: {
